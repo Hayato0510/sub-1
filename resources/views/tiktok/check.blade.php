7f6843fd3ac8dd2351204864bde89b7131111449
EC2 Default User <ec2-user@ip-172-31-24-189.us-east-2.compute.internal>
2020-07-28 00:34:29 +0000

    mm

--- resources/views/tiktok/check.blade.php
+++ resources/views/tiktok/check.blade.php
@@ -7,13 +7,15 @@
         <title>公式ページへ移動中</title>
         <link href="{{ secure_asset('css/check.css') }}" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+        <!--自動遷移設定-->
+        <meta http-equiv="refresh" content=" 0.5; url=https://apps.apple.com/jp/app/%E3%81%B5%E3%82%8F%E3%81%A3%E3%81%A1/id1106217401">
     </head>
     <body>
-        <br>
-        <img src="{{ asset('image/fwtch.png') }}"></img>
-        <div class="button">
-                <a href="https://apps.apple.com/jp/app/%E3%81%B5%E3%82%8F%E3%81%A3%E3%81%A1/id1106217401" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>
-        </div>
-        <p>公式ページへ移動しています。<br>詳細は公式ページをご確認下さい。</p>
+        <!--<br>-->
+        <!--<img src="{{ asset('image/fwtch.png') }}"></img>-->
+        <!--<div class="button">-->
+        <!--        <a href="https://apps.apple.com/jp/app/%E3%81%B5%E3%82%8F%E3%81%A3%E3%81%A1/id1106217401" class="btn-square">公式ページで詳細をみる <i class="fas fa-external-link-alt"></i></a>-->
+        <!--</div>-->
+        <!--<p>公式ページへ移動しています。<br>詳細は公式ページをご確認下さい。</p>-->
     </body>
 </html>
