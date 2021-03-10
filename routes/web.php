521d7d28f5537079c9feba15b90864f8f8682a95
EC2 Default User <ec2-user@ip-172-31-24-189.us-east-2.compute.internal>
2021-03-10 12:03:49 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 //LP獲得用
 Route::get('app1', function () {
-    return view('tiktok.check'); //仮LP
-    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
+    // return view('tiktok.check'); //仮LP
+    return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
     
     // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
     // return view('tiktok.job'); //獲得用(2)
