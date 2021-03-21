b11f96d6d8f4089b22e8044794cd9a658c517c0b
EC2 Default User <ec2-user@ip-172-31-24-189.us-east-2.compute.internal>
2021-03-21 01:42:27 +0000

    m

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class AdController extends Controller
 {
     //
+    public function info(Request $request) {
+        $user_agent =  $request->header('User-Agent');
+        if ((strpos($user_agent, 'iPhone') !== false)) {
+            return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活iOS - fam
+        }
+        return redirect('https://fam-ad.com/ad/p/r?_site=64802&_article=16345'); //ワクワクP活web - fam
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,20 +13,24 @@
 //LP獲得用
 Route::get('app1', function () {
     return view('tiktok.check'); //仮LP
-    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
+    //リダイレクトLPへ切り替え - cccheckを変更
     
+    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
     // return view('tiktok.wklp'); //獲得用(3) - ワクワクP活LP
     // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
     // return view('tiktok.job'); //獲得用(2)
     // return view('tiktok.noMatch'); //獲得用(1)
 });
 
-//LP確認用
-Route::get('cccheck', function () {
-    return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
+// OS識別x遷移 for パパ活
+Route::get('cccheck', 'AdController@info');
+
+// //LP確認用
+// Route::get('cccheck', function () {
+//     return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
     
-    // return view('tiktok.wklp'); //獲得用(3) - ワクワクP活LP
-    // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
-    // return view('tiktok.job'); //獲得用(2)
-    // return view('tiktok.noMatch'); //獲得用(1)
-});
+//     // return view('tiktok.wklp'); //獲得用(3) - ワクワクP活LP
+//     // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
+//     // return view('tiktok.job'); //獲得用(2)
+//     // return view('tiktok.noMatch'); //獲得用(1)
+// });
