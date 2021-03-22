c3bd905a484fcb286e73da9f088b0935359f2cb5
EC2 Default User <ec2-user@ip-172-31-24-189.us-east-2.compute.internal>
2021-03-22 00:20:07 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,20 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-// //LP獲得用
-// Route::get('app1', function () {
-//     return view('tiktok.check'); //仮LP
-//     //リダイレクトLPへ切り替え - cccheckを変更
+//LP獲得用
+Route::get('app1', function () {
+    return view('tiktok.check'); //仮LP
+    //リダイレクトLPへ切り替え - cccheckを変更
     
-//     // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
-//     // return view('tiktok.wklp'); //獲得用(3) - ワクワクP活LP
-//     // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
-//     // return view('tiktok.job'); //獲得用(2)
-//     // return view('tiktok.noMatch'); //獲得用(1)
-// });
+    // return redirect('https://fam-ad.com/ad/p/r?_site=64801&_article=16355'); //ワクワクP活LP
+    // return view('tiktok.wklp'); //獲得用(3) - ワクワクP活LP
+    // return redirect('https://a-trade.jp/redirect/mocom-lp30?media=G16531'); //モコム - Aトレード
+    // return view('tiktok.job'); //獲得用(2)
+    // return view('tiktok.noMatch'); //獲得用(1)
+});
 
 // OS識別x遷移 for パパ活
-Route::get('app1', 'AdController@info');
+Route::get('cccheck', 'AdController@info');
 
 // //LP確認用
 // Route::get('cccheck', function () {
